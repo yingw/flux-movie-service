9aadd80deb595cf02950f5c1f1cc4dffd951f6a5
yinguowei <yinguowei@gmail.com>
2018-01-20 15:08:31 +0800

    first init.

---
+++ src/main/java/com/example/fluxmovieservice/FluxMovieServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.fluxmovieservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxMovieServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluxMovieServiceApplication.class, args);
+	}
+}
